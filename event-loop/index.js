fe04b890859761de84fe3e1a5e0e0e13cd220694
Paulo Victor <paulovictordev01@gmail.com>
2025-11-28 11:45:33 -0300

    Event loop no nodeJS, exemplo de callback de forma assincrona

--- event-loop/index.js
+++ event-loop/index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs')
 console.log('Inicio')
 
-fs.readFile('./arquivo.txt', 'utf8', (err, data) => {
+fs.readFile('./arquivo.txt', 'utf8', (err, data) => { // Lendo o arquivo de forma ASSÍNCRONA, callback(err, data) => {}
   if(err) throw err
   console.log(data)
 })
