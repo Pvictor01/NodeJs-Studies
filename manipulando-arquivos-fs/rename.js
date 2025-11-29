1c164420cf01d89857d71c27f21f447a70d00918
Paulo Victor <paulovictordev01@gmail.com>
2025-11-28 21:34:02 -0300

    Renomeando arquivos, correção do console.log

--- manipulando-arquivos-fs/rename.js
+++ manipulando-arquivos-fs/rename.js
@@ -1,9 +1,8 @@
-import { log } from 'node:console'
 import fs from 'node:fs'
 
 fs.rename('./arquivo.txt', './arquivo-renomeado.txt', (err) => {
   if(err) {
-    log(`Erro ao renomear arquivo ${err.message}`)
+    console.log(`Erro ao renomear arquivo ${err.message}`)
     return
   }
 
