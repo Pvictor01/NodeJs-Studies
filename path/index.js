324d2cc3658df339d66c3752b19be4480432de54
Paulo Victor <paulovictordev01@gmail.com>
2025-12-09 16:54:55 -0300

    Modulo Path, usando o resolve

--- path/index.js
+++ path/index.js
@@ -4,6 +4,11 @@ const dir = 'src'
 const file = 'arquivo.js'
 
 const fullPath = path.join(__dirname, dir, file)   //juntar todos os argumentos em um unico caminho, dirname pega o caminho absoluto
-const relativePath = path.join('.', dir, file) //mostra apenas os arquivos do diretorio atual, src/arquivo.js
-console.log(fullPath)
-console.log(relativePath)
+//const relativePath = path.join('.', dir, file) //mostra apenas os arquivos do diretorio atual, src/arquivo.js
+
+//console.log(fullPath)
+
+const relativePath = '../arquivos/relatorio.pdf'
+
+const absolutPath = path.resolve(__dirname, relativePath) //ira mostrar o caminho completo, menos a pasta path, pois pulou essa pasta com ../
+console.log(absolutPath)
