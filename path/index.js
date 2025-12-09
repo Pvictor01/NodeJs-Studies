44d39ec2972fe39318c4bd7d5fc2e946a19b74ce
Paulo Victor <paulovictordev01@gmail.com>
2025-12-09 16:58:23 -0300

    Modulo Path, usando o __filename

--- path/index.js
+++ path/index.js
@@ -3,12 +3,15 @@ const path = require('node:path')
 const dir = 'src'
 const file = 'arquivo.js'
 
-const fullPath = path.join(__dirname, dir, file)   //juntar todos os argumentos em um unico caminho, dirname pega o caminho absoluto
-//const relativePath = path.join('.', dir, file) //mostra apenas os arquivos do diretorio atual, src/arquivo.js
+/*const fullPath = path.join(__dirname, dir, file)   //juntar todos os argumentos em um unico caminho, dirname pega o caminho absoluto
+const relativePath = path.join('.', dir, file) //mostra apenas os arquivos do diretorio atual, src/arquivo.js
 
-//console.log(fullPath)
+console.log(fullPath)*/
 
-const relativePath = '../arquivos/relatorio.pdf'
+/*const relativePath = '../arquivos/relatorio.pdf'
 
 const absolutPath = path.resolve(__dirname, relativePath) //ira mostrar o caminho completo, menos a pasta path, pois pulou essa pasta com ../
-console.log(absolutPath)
+console.log(absolutPath)*/
+
+const fileName = path.basename(__filename) //retorna a ultima parte de um caminho
+console.log(fileName)
