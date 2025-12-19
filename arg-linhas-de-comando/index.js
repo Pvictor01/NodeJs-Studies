5ada65c6b287b9b97a80bc0fe3d7c9e76201255f
Paulo Victor <paulovictordev01@gmail.com>
2025-12-18 23:58:38 -0300

    Add comentarios para consultas

--- arg-linhas-de-comando/index.js
+++ arg-linhas-de-comando/index.js
@@ -5,7 +5,7 @@ const args = process.argv //argv é uma lista dos arg da linha de comando
 const namedArguments = {}
 
 process.argv.slice(2).forEach((arg, index, arr) => { //slice retira os dois primeiros argumentos padrao
-  if(arg.startsWith('--')) { //perga o item do arr que começa com --
+  if(arg.startsWith('--')) { //pega o item do arr que começa com --
     const argName = arg.slice(2) //retira os dois -- e pega so a string
     const argValue =  arr[index + 1]  //pega o proximo valor do item que começa com --
     namedArguments[argName] = argValue //insere chave e valor ao objeto
